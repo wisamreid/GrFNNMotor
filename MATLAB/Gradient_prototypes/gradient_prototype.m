f7691bbe7c558cd586fd01f9a91848cdb6f910bd
iranroman <iran@stanford.edu>
2016-12-02 21:35:20 -0800

    gradient troubleshooting'

--- MATLAB/Gradient_prototypes/gradient_prototype.m
+++ MATLAB/Gradient_prototypes/gradient_prototype.m
@@ -11,13 +11,11 @@ alpha = 1;
 beta1 = -1;
 beta2 = -1;
 epsilon = 1;
-w = 2*pi;
 
 % parameters for the input
 F = 1;
 f = 1;
 w0 = 2*pi*f;
-omega = w - w0;
     
 psi0 = 0.5*pi;
 
@@ -30,6 +28,8 @@ time = 0:T:end_t;
 for i=1:nfreqs
     
     f_scale = freqs(i);
+    w = 2*pi*f_scale;
+    omega = w - w0;
     
     % solve r_dot
     full_sys = @(t,z,alpha,beta1,beta2,epsilon,w,omega,F,w0,f_scale)  ...
@@ -47,18 +47,13 @@ for i=1:nfreqs
     grid on
     xlabel('Time (s)')
     ylabel('Magnitude')
-    subplot(3,nfreqs,nfreqs/1+i)
+    subplot(3,nfreqs,nfreqs+i)
     plot(time,(1/f_scale)*z(:,2))
     grid on
     xlabel('Time (s)')
     ylabel('Relative Phase')
-    
-%     figure(2)
-%     polar(z(:,2),z(:,1))
-%     hold on
-%     
-%     figure(3)
-%     plot3(time,real(z(:,1).*exp(1i*z(:,2))),imag(z(:,1).*exp(1i*z(:,2))))
-%     grid on
+    subplot(3,nfreqs,nfreqs*2+i)
+    polar((1/f_scale)*z(:,1),(1/f_scale)*z(:,2))
+    grid on
     
 end
