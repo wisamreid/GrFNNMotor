962fd8e8d6519f107cb4b8280f6991e02c3645fc
Wisam Reid <wisamreid@gmail.com>
2016-12-09 20:07:06 -0800

    Adding Steady State Scripts

---
+++ MATLAB/Plotting_SS.m
@@ -0,0 +1,100 @@
+% Replicating Figure 1: Kim and Large 2015
+% by Wisam Reid
+
+%% CLEAN AND CLEAR
+
+clc;
+clear;
+close all;
+
+%% equations to solve
+
+% two oscillators with recurrent coupling
+drdt = @(t,r,alpha1,beta11,beta12,epsilon1, ... % osc 1 params
+             alpha2,beta21,beta22,epsilon2, ... % osc 2 params
+             alpha3,beta31,beta32,epsilon3, ... % osc 3 params
+             alpha4,beta41,beta42,epsilon4) ... % osc 4 params 
+    ... % osc 1
+    [alpha1*r(1) + beta11*r(1)^3 + (epsilon1*beta12*r(1)^5)/(1-epsilon1*r(1)^2); ...
+    ... % osc 2
+     alpha2*r(2) + beta21*r(2)^3 + (epsilon2*beta22*r(2)^5)/(1-epsilon2*r(2)^2);
+    ... % osc 3
+     alpha3*r(3) + beta31*r(3)^3 + (epsilon3*beta32*r(3)^5)/(1-epsilon3*r(3)^2);
+    ... % osc 4
+     alpha4*r(4) + beta41*r(4)^3 + (epsilon4*beta42*r(4)^5)/(1-epsilon4*r(4)^2)];
+ 
+%% Oscillator parameters
+
+% oscillator 1 parameters: a critical Hopf regime
+%   a = 0, b1 < 0, b2 = 0
+r01 = 0.2;
+alpha1 = 0;
+beta11 = -1.0;
+beta12 = 0;
+epsilon1 = 0;
+
+% oscillator 2 parameters: a supercritical Hopf regime 
+%   a > 0, b1 < 0, b2 = 0
+r02 = 0.2;
+alpha2 = 1.0;
+beta21 = -1.0;
+beta22 = 0;
+epsilon2 = 0;
+
+% oscillator 3 parameters: a super critical double limit cycle regime
+%   a < 0, b1 > 0, b2 < 0, localmax > 0
+r03 = 1;
+alpha3 = -1;
+beta31 = 1;
+beta32 = -1;
+epsilon3 = 1;
+
+% oscillator 4 parameters: a subcritical double limit cycle regime: 
+%   a < 0, b1 > 0, b2 < 0, local max < 0
+r04 = 1;
+alpha4 = -1;
+beta41 = 1;
+beta42 = -10;
+epsilon4 = 1;
+
+%% time parameters
+
+fs = 5000;
+dur = 10; % in seconds
+T = 1/fs;
+time = 0:T:dur;
+
+%% Integrate the ode to obtain the amplitude
+
+[t,r] = ode45(@(t,r) ...
+        drdt(t,r,alpha1,beta11,beta12,epsilon1,  ... % osc 1 params
+                 alpha2,beta21,beta22,epsilon2,  ... % osc 2 params
+                 alpha3,beta31,beta32,epsilon3,  ... % osc 3 params
+                 alpha4,beta41,beta42,epsilon4), ... % osc 4 params
+                 time,[r01,r02,r03,r04]);
+
+%% grab the derivative
+
+drdt = diff(r);
+
+%% Plot Responses
+
+figure(1)
+% critical Hopf
+plot(r(1:end-1,1),drdt,'-k','LineWidth',2)
+hold on
+% super critical Hopf
+plot(r(1:end-1,2),drdt,'-r','LineWidth',2)
+hold on
+% super critical double limit cycle
+plot(r(1:end-1,3),drdt,'-g','LineWidth',2)
+hold on
+% double limit cycle (subcritical)
+plot(r(1:end-1,4),drdt,'-b','LineWidth',2)
+hold on
+line(xlim,[0 0],'Color','c','LineStyle','--') 
+title('Autonomous behavior in different parameter regimes')
+xlabel('$r$','Interpreter','Latex')
+ylabel('$\frac{dr}{dt}$','Interpreter','Latex')
+legend('Critical Hopf','Super Critical Hopf','SC Double Limit Cycle', ...
+    'Sub-Critical DLC','Location', 'South')
---
+++ MATLAB/stable_steady_sate.m
@@ -0,0 +1,236 @@
+%%
+
+clear all;close all;clc;
+
+animate = 1; % true or false
+
+% parameters of the gamma oscillator
+z0 = 0.1;
+alpha = 1;
+beta1 = -100;
+beta2 = 0;
+epsilon = 0;
+f1 = 1;
+
+% parameters for the input
+F = 0.2;
+f0 = 1.55;
+w0 = 2*pi*f0;
+
+% parameters for time
+fs = 1000;
+end_t = 100; % in seconds
+T = 1/fs;
+time = 0:T:end_t;
+
+dzdt = @(t,z,alpha,beta1,beta2,epsilon,f1,F,w0)  ...
+    z*(alpha + 1i*2*pi*f1 + beta1*abs(z)^2 + ...
+    (epsilon*beta2*abs(z)^4)/(1-epsilon*abs(z)^2)) + F*exp(1i*w0*t);
+
+% integrate the ode to obtain the amplitude
+[time,z_out_1] = ode45(@(t,z) dzdt(t,z,alpha,beta1,beta2,epsilon,f1,F,w0),time,z0);
+
+z_all = [];
+time_all = [];
+z_all = [z_all z_out_1'];
+time_all = [time_all time'];
+
+figure(1)
+subplot(3,2,1:2)
+plot(time_all,abs(z_all))
+grid on
+xlabel('Time (s)')
+xlabel('Magnitude')
+
+if animate == 1
+    
+    for i=1:length(time_all)
+        
+        if mod(i,fs/20) == 0
+            subplot(3,2,3:6)
+            plot3(time_all(1:i),real(z_all(1:i)),imag(z_all(1:i)),'-')
+            grid on
+            xlabel('Time (s)')
+            ylabel('Real part')
+            zlabel('Imaginary part')
+            pause(0.1)
+            
+        end
+    end
+    
+else
+    
+    figure(2)
+    plot3(time_all,real(z_all),imag(z_all))
+    hold on
+    grid on
+    xlabel('Time (s)')
+    ylabel('Real part')
+    zlabel('Imaginary part')
+    
+end
+
+%%
+clear all;close all;clc;
+
+% parameters of the gamma oscillator
+r0 = 0.2;
+alpha = 0;
+beta1 = -100;
+beta2 = 0;
+epsilon = 0;
+f = 1;
+w = f*2*pi;
+
+% parameters for the input
+F = 0.2;
+f0 = 3;
+w0 = 2*pi*f0;
+
+omega = w - w0;
+psi0 = pi;
+
+% parameters for time
+fs = 1000;
+end_t = 100; % in seconds
+T = 1/fs;
+time = 0:T:end_t;
+
+% solve r_dot
+full_sys = @(t,z,alpha,beta1,beta2,epsilon,w,omega,F,w0)  ...
+    [z(1)*alpha + beta1*z(1)^3 + (epsilon*beta2*z(1)^5)/(1-epsilon*z(1)^2) + F*cos(z(2));
+    omega - (F/z(1))*sin(z(2))];   
+
+% integrate the ode to obtain the amplitude
+[time,r] = ode45(@(t,z) full_sys(t,z,alpha,beta1,beta2,epsilon,w,omega,F,w0),time,[r0,psi0]);
+
+figure(1)
+subplot(2,1,1)
+plot(time,r(:,1))
+grid on
+xlabel('Time (s)')
+ylabel('Magnitude')
+subplot(2,1,2)
+plot(time,r(:,2))
+grid on
+xlabel('Time (s)')
+ylabel('Relative Phase')
+
+figure(2)
+polar(r(:,2),r(:,1))
+hold on
+
+figure(3)
+plot3(time,r(:,1).*real(exp(1i*r(:,2))),r(:,1).*imag(exp(1i*r(:,2))))
+% plot3(time(fs*5:end),r(fs*5:end,1).*real(exp(1i*r(fs*5:end,2))),r(fs*5:end,1).*imag(exp(1i*r(fs*5:end,2))))
+grid on
+
+%%
+clear all;close all;clc;
+
+animate = 1; % true or false
+
+% parameters of the gamma oscillator
+r0 = 0.05;
+alpha = 1;
+beta1 = -100;
+beta2 = 0;
+epsilon = 0;
+f = 1;
+w = f*2*pi;
+
+% parameters for the input
+F = 0.2;
+f0 = 0.3;
+w0 = 2*pi*f0;
+
+omega = w - w0;
+psi0 = 0.9*pi;
+
+% parameters for time
+fs = 1000;
+end_t = 10; % in seconds
+T = 1/fs;
+time = 0:T:end_t;
+
+% solve r_dot
+full_sys = @(t,z,alpha,beta1,beta2,epsilon,w,omega,F,w0)  ...
+    [z(1)*alpha + beta1*z(1)^3 + (epsilon*beta2*z(1)^5)/(1-epsilon*z(1)^2) + F*cos(z(2));
+    omega - (F/z(1))*sin(z(2))];   
+
+% integrate the ode to obtain the amplitude
+[time,r] = ode45(@(t,z) full_sys(t,z,alpha,beta1,beta2,epsilon,w,omega,F,w0),time,[r0,psi0]);
+
+figure(1)
+subplot(2,1,1)
+plot(time,r(:,1))
+grid on
+xlabel('Time (s)')
+ylabel('Magnitude')
+subplot(2,1,2)
+plot(time,r(:,2))
+grid on
+xlabel('Time (s)')
+ylabel('Relative Phase')
+
+figure(2)
+polar(r(:,2),r(:,1))
+hold on
+
+figure(3)
+plot3(time,real(r(:,1).*exp(1i*r(:,2))),imag(r(:,1).*exp(1i*r(:,2))))
+grid on
+
+%%
+clear all;close all;clc;
+
+% parameters of the gamma oscillator
+r0 = 0.9;
+alpha = -1;
+beta1 = 4;
+beta2 = -1;
+epsilon = 1;
+f = 1;
+w = f*2*pi;
+
+% parameters for the input
+F = 0.3;
+f0 = 0.8;
+w0 = 2*pi*f0;
+
+omega = w - w0;
+psi0 = 0.9*pi;
+
+% parameters for time
+fs = 1000;
+end_t = 20; % in seconds
+T = 1/fs;
+time = 0:T:end_t;
+
+% solve r_dot
+full_sys = @(t,z,alpha,beta1,beta2,epsilon,w,omega,F,w0)  ...
+    [z(1)*alpha + beta1*z(1)^3 + (epsilon*beta2*z(1)^5)/(1-epsilon*z(1)^2) + F*cos(z(2));
+    omega - (F/z(1))*sin(z(2))];   
+
+% integrate the ode to obtain the amplitude
+[time,r] = ode45(@(t,z) full_sys(t,z,alpha,beta1,beta2,epsilon,w,omega,F,w0),time,[r0,psi0]);
+
+figure(1)
+subplot(2,1,1)
+plot(time,r(:,1))
+grid on
+xlabel('Time (s)')
+ylabel('Magnitude')
+subplot(2,1,2)
+plot(time,r(:,2))
+grid on
+xlabel('Time (s)')
+ylabel('Relative Phase')
+
+figure(2)
+polar(r(:,2),r(:,1))
+hold on
+
+figure(3)
+plot3(time,real(r(:,1).*exp(1i*r(:,2))),imag(r(:,1).*exp(1i*r(:,2))))
+grid on
